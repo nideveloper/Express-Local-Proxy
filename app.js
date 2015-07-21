35445963cdeb6cfe0243b8108d28fd8337bf0451
Matt Coulter <matt@builtbyfuse.com>
2015-07-21 14:40:01 +0100

    Added in proxy support and twitter integration

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
 var express = require('express');
+var nconf = require('nconf');
 var app = express();
+
+nconf.argv()
+       .env()
+       .file({ file: 'config.json' });
 
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
@@ -16,49 +21,38 @@ app.get('/', function (req, res) {
     })
 });
 
-app.get('/categories', function (req, res) {
+app.get('/api/*', function (req, res) {
     var request = require('request');
-    request('http://nideveloper.co.uk/api/categories', function (error, response, body) {
-      if (!error && response.statusCode == 200) {
-        res.send(body);
-      }
-    })
+    var r = request.defaults({ 'proxy': nconf.get('PROXY') });
+
+    console.log("Calling URL " + req.url);
+    r.get('http://nideveloper.co.uk' + req.url).pipe(res);
 });
 
-app.get('/category/:categoryID', function (req, res) {
-    var request = require('request');
-    request('http://nideveloper.co.uk/api/category/'+req.params.categoryID, function (error, response, body) {
-      if (!error && response.statusCode == 200) {
-        res.send(body);
-      }
-    })
-});
+app.get('/twitter/nideveloper', function (req, res) {
+    var Twitter = require('twitter');
 
-app.get('/posts', function (req, res) {
-    var request = require('request');
-    request('http://nideveloper.co.uk/api/posts', function (error, response, body) {
-      if (!error && response.statusCode == 200) {
-        res.send(body);
-      }
-    })
-});
+    var client = new Twitter({
+        consumer_key: nconf.get('TWITTER_CONSUMER_KEY'),
+        consumer_secret: nconf.get('TWITTER_CONSUMER_SECRET'),
+        access_token_key: nconf.get('TWITTER_ACCESS_TOKEN_KEY'),
+        access_token_secret: nconf.get('TWITTER_ACCESS_TOKEN_SECRET'),
+        request_options: {
+            proxy: nconf.get('PROXY')
+        }
+    });
 
-app.get('/post/:id', function (req, res) {
-    var request = require('request');
-    request('http://nideveloper.co.uk/api/post/'+req.params.id, function (error, response, body) {
-      if (!error && response.statusCode == 200) {
-        res.send(body);
-      }
-    })
-});
+    console.log("Loading NI Developer Tweets");
 
-app.get('/search/:query', function (req, res) {
-    var request = require('request');
-    request('http://nideveloper.co.uk/api/search/'+req.params.query, function (error, response, body) {
-      if (!error && response.statusCode == 200) {
-        res.send(body);
-      }
-    })
+    var params = { screen_name: 'nideveloper', count: 5 };
+
+    client.get('statuses/user_timeline', params, function (error, tweets, response) {
+        if (!error) {
+            res.json(tweets);
+        } else {
+            console.log(error)
+        }
+    });
 });
 
 var server = app.listen(3000, function () {
